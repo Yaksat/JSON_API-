b55881637a322effa44470e5e015329b23f1de2d
Damir Aubekerov <80846714+Yaksat@users.noreply.github.com>
2022-09-06 17:03:58 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+//
 Route::view('/', 'home')->name('workers.home');
 Route::get('/workers', \App\Http\Controllers\IndexController::class)->name('workers.index');
 Route::get('/workers/create', \App\Http\Controllers\CreateController::class)->name('workers.create');
